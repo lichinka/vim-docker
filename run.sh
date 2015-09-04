41aa6bdaf13b572c918129ec073f9a1366dc4a9c
Lucas Benedicic <benedicic@cscs.ch>
2015-09-04 10:41:16 +0200

    Full paths are mounted directly.-

--- run.sh
+++ run.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
 
-MNT_DIR="$( pwd )"
+SRC_FILE="$1"
 MNT_PNT="/home/dev/src"
 
+get_realpath()
+{
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 #
-# try to mount a Git repository root if no parameter were given
+# open the file explorer if nothing was given
 #
-if [ ! -d ".git" ]; then
+if [ -z "${SRC_FILE}" ]; then
+    MNT_DIR="$( pwd )"
+    TGT_FILE="."
+#
+# full paths should be mounted directly
+#
+elif [[ ${SRC_FILE} == "/"* ]]; then
+    MNT_DIR="$( dirname ${SRC_FILE} )"
+    TGT_FILE="${MNT_PNT}/$( basename ${SRC_FILE} )"
+#
+# relative paths are handled differently
+#
+elif [ ! -d ".git" ]; then
     git rev-parse --git-dir > /dev/null 2>&1
+    #
+    # try to mount a Git repository
+    #
     if [ $? -eq 0 ]; then
         MNT_DIR="$( git rev-parse --git-dir | sed -e 's/git$//' )"
+        MNT_DIR="$( get_realpath "${MNT_DIR}" )"
+        MNT_DIR="$( dirname ${MNT_DIR} )"
+        TGT_FILE="$( basename ${SRC_FILE} )"
     fi
 fi
+#
+# or just the current directory if everything else fails
+#
+if [ -z "${MNT_DIR}" ] && [ -z "${TGT_FILE}" ]; then
+    #
+    # or just the current directory
+    #
+    MNT_DIR="$( pwd )"
+    TGT_FILE="${MNT_PNT}/$( basename ${SRC_FILE} )"
+fi
 
-echo "Mounting directory '${MNT_DIR}' under '${MNT_PNT}'"
+echo "Opening file '${SRC_FILE}' as '${TGT_FILE}'"
 docker run --rm                      \
            -it                       \
            --env HOST_UID=$( id -u ) \
            --env HOST_GID=$( id -g ) \
            -v ${MNT_DIR}:${MNT_PNT}  \
            vim-devel:latest          \
-           vim $@
+           vim ${TGT_FILE}
